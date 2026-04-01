7e2cdb31386d8f3720c143f13d850ccd05163fa0
Thiztoon <jittapongj@gmail.com>
2026-04-02 02:10:53 +0700

    update: justify homepage

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,13 +3,13 @@ import '../styles/home.scss'
 
 function Home() {
     return (
-        <section id="homepage" className="flex justify-center items-center gap-8">
+        <section id="homepage" className="flex flex-col justify-center items-center gap-8">
             <div className="profile overflow-hidden ">
                 <img src={profileImg} alt="Profile" className="profile-image" />
             </div>
             <div class="content-info">
-                <h1>Jittapong Jongjai</h1>
-                <h3>About Me</h3>
+                <h1 className='flex justify-center mb-4'>Jittapong Jongjai</h1>
+                <h3 className='flex justify-center'>About Me</h3>
                 <p>Hello, my nickname is Toon, 24 years old, currently studying in the Faculty of Engineering, Computer
                     Science Program, Rajamangala University of Technology Lanna, Chiang Mai. I am interested in learning and
                     developing skills in the field of Front-end Developer, especially in HTML PHP jQuery and SCSS. I have
